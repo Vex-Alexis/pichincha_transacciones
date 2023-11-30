60dac673cb78175a920fdfff2372f8a3c9d1ecaf
Alexis Chavarria <joalexiscb@gmail.com>
2023-11-30 01:08:40 -0500

    Primer commit

---
+++ src/main/java/com/pichincha/transacciones/TransaccionesServicioApplication.java
@@ -0,0 +1,13 @@
+package com.pichincha.transacciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransaccionesServicioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransaccionesServicioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pichincha/transacciones/TransaccionesServicioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pichincha.transacciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransaccionesServicioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
